ad32da3db9d63e66b6eab3b1b11bb1a624319157
Iryna <tichaya.13@gmail.com>
2023-03-27 01:11:43 +0300

    Task16 Решение задачи

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,3 +6,16 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsEqualSquared(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool IsEqualSquared(int num1, int num2)
+{
+   return num1 * num1 == num2  || num2 * num2 == num1;
+}
