6e3155871ac0f9bbf8a1ed65544ded2e93a789d1
Iryna <tichaya.13@gmail.com>
2023-03-27 00:50:57 +0300

    Task14 Решение задачи

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,3 +5,14 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultTwoNumbers(number, 7, 23);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool MultTwoNumbers(int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
