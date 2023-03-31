4a0eb147d63142ff5735f1d1cd06469b0827b217
Iryna <tichaya.13@gmail.com>
2023-03-31 16:04:18 +0300

    Task18 с оптимальным  решением  задачи

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,35 +1,51 @@
 ﻿// Задача 18: Напишите программу, которая 
 // по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-Console.Write("Введите номер четверти от 1 до 4: ");
+Console.WriteLine("Введите номер четверти");   // Самое оптимальное решение, которое предложено на занятии
+string quarterNum = Console.ReadLine();
 
-//int number = Convert.ToInt32(Console.ReadLine()); - нет необходимости преобразовывать ввод пользователя в цифру. Тогда см. строку 8
+string result = RangeCoord(quarterNum);
+Console.WriteLine(result);
 
-string result = Console.ReadLine();  // к этому условию - смотри второй метод
+string RangeCoord (string quarter)
+{
+if (quarter == "1") return "x > 0, y > 0";
+if (quarter == "2") return "x < 0, y > 0";
+if (quarter == "3") return "x < 0, y < 0";
+if (quarter == "4") return "x > 0, y < 0";
+return "Введена некорректно четверть";
+}
 
-string num = RangeCoordinate(result);
-Console.WriteLine(num);
+// Console.Write("Введите номер четверти от 1 до 4: ");
 
-// string number1 = RangeCoordinate(number);
-// string result = number < 5 && number > 0 
-// ? $"Диапазон возможных координат точек в этой четверти: {number1}"
-// : "Неверно";
-// Console.WriteLine(result);
+// //int number = Convert.ToInt32(Console.ReadLine()); - нет необходимости преобразовывать ввод пользователя в цифру. Тогда см. строку 8
 
-// // string RangeCoordinate (int num)
+// string result = Console.ReadLine();  // к этому условию - смотри второй метод
+
+// string num = RangeCoordinate(result);
+// Console.WriteLine(num);
+
+// // string number1 = RangeCoordinate(number);
+// // string result = number < 5 && number > 0 
+// // ? $"Диапазон возможных координат точек в этой четверти: {number1}"
+// // : "Неверно";
+// // Console.WriteLine(result);
+
+// // // string RangeCoordinate (int num)
+// // {
+// //     if (num == 1) return "x > 0, y > 0";
+// //     if (num == 2) return "x < 0, y > 0";
+// //     if (num == 3) return "x < 0, y < 0";
+// //     if (num == 4) return "x > 0, y < 0";
+// //     return "Номер четверти введён некорректно";
+// // }
+
+// string RangeCoordinate (string num)
 // {
-//     if (num == 1) return "x > 0, y > 0";
-//     if (num == 2) return "x < 0, y > 0";
-//     if (num == 3) return "x < 0, y < 0";
-//     if (num == 4) return "x > 0, y < 0";
+//     if (num == "1") return "x > 0, y > 0";
+//     if (num == "2") return "x < 0, y > 0";
+//     if (num == "3") return "x < 0, y < 0";
+//     if (num == "4") return "x > 0, y < 0";
 //     return "Номер четверти введён некорректно";
 // }
 
-string RangeCoordinate (string num)
-{
-    if (num == "1") return "x > 0, y > 0";
-    if (num == "2") return "x < 0, y > 0";
-    if (num == "3") return "x < 0, y < 0";
-    if (num == "4") return "x > 0, y < 0";
-    return "Номер четверти введён некорректно";
-}
