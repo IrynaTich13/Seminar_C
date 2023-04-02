072d76dd2e1caadd77510c0ddf9f8540e26f1c38
Iryna <tichaya.13@gmail.com>
2023-04-02 18:41:27 +0300

    Решение задачи

---
+++ Task19/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая 
+// принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = number * -1;
+if (number > 9999 && number < 100000)
+{
+    bool result = Palindrom(number);
+    Console.WriteLine(result ? "Введённое число является палиндромом" : "Введённое число НЕ является палиндромом");
+}
+else
+    Console.Write("Вы ввели не пятизначное число");
+
+bool Palindrom(int num)
+{
+    int firstnum = num;
+    int reversNum = 0;
+
+    while (num > 0)
+    {
+        reversNum = reversNum * 10 + num % 10;
+        num /= 10;
+    }
+    return reversNum == firstnum;
+}
+
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 0) num = num * -1;
+// if (num < 10000 || num > 100000)
+// {
+//     Console.WriteLine("Вы ввели не пятизначное число");
+// }
+// else
+// {
+//     Palindrome(num);
+//     void Palindrome(int number)
+//     {
+//         if ((num / 10000 == num % 10) && ((num / 1000) % 10 == (num % 100) / 10))
+//             Console.WriteLine("Введённое число является палиндромом");
+
+//         else Console.WriteLine("Введённое число НЕ является палиндромом");
+//     }
+// }
+
+//(num / 10000 == num % 10) - сраниваем 1 и 5 цифры
+//(num / 1000) % 10 == (num % 100) / 10) - сравниваем 2 и 4 цифры
