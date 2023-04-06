06d698bfabcf980b4c422c5dd9a2a2d12c3ffde1
Iryna <tichaya.13@gmail.com>
2023-04-06 19:42:30 +0300

    Task33 - Решение задачи

---
+++ Task33/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] array = CreateArreyRndInt(8, -100, 100);
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = SearcheNum(array, num) == true ? "Да, заданное число присутствует в массиве" 
+                                               : "Нет, заданного числа в массиве нет";
+
+Console.WriteLine(result);
+
+int[] CreateArreyRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)      // либо for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+bool SearcheNum(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
