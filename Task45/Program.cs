446db9a20de224bb42e15bc10e87cd7eccdd8e5f
Iryna <tichaya.13@gmail.com>
2023-04-09 02:42:44 +0300

    Task44 Решение задачи

---
+++ Task45/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 45: Напишите программу, которая 
+// будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+int[] array = CreateArreyRndInt(7, 0, 10);
+PrintArray(array);
+
+Console.WriteLine();
+int[] copyArr = CopyArray(array);
+//  copyArr[0] = 12; - это проверка на то, что мы действительно создали копию, а не скопировали ссылку
+PrintArray(copyArr);
+
+
+int[] CreateArreyRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)      // либо for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CopyArray (int[] arr)
+{
+    int[] copyArrey = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+      copyArrey[i] = arr[i]; 
+    }
+    return copyArrey;
+}
