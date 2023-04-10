3e5df3998cb20419a7fb39f618cae4d45b8104ab
Iryna <tichaya.13@gmail.com>
2023-04-11 00:05:53 +0300

    Task45 Дополнила решение задачи другим способом

--- Task45/Program.cs
+++ Task45/Program.cs
@@ -45,57 +45,57 @@ int[] CopyArray (int[] arr)
 }
 /////////////////////////////////////////////////////////
 
-int[] myArray = CreateArrayRndInt(7, 1, 10);
-int[] newArr = CopyArray(myArray);
-newArr[0] = 20;
+// int[] myArray = CreateArrayRndInt(7, 1, 10);
+// int[] newArr = CopyArray(myArray);
+// newArr[0] = 20;
 
-Console.WriteLine("\nИсходный массив");
-PrintArray(myArray);
+// Console.WriteLine("\nИсходный массив");
+// PrintArray(myArray);
 
-Console.WriteLine("Новый массив");
-PrintArray(newArr);
+// Console.WriteLine("Новый массив");
+// PrintArray(newArr);
 
-bool res = Equal(myArray, newArr);
-Console.WriteLine(res ? "Равны" : "Не равны");
+// bool res = Equal(myArray, newArr);
+// Console.WriteLine(res ? "Равны" : "Не равны");
 
-bool Equal(int[] arr1, int[] arr2)
-{
-if(arr1.Length != arr2.Length)
-{
-return false;
-}
+// bool Equal(int[] arr1, int[] arr2)
+// {
+// if(arr1.Length != arr2.Length)
+// {
+// return false;
+// }
 
-for (int i = 0; i < arr1.Length; i++)
-{
-if(arr1[i] != arr2[i])
-{
-return false;
-}
-}
-return true;
-}
+// for (int i = 0; i < arr1.Length; i++)
+// {
+// if(arr1[i] != arr2[i])
+// {
+// return false;
+// }
+// }
+// return true;
+// }
 
-int[] CopyArray(int[] array)
-{
-int[] newArray = new int[array.Length];
-for (int i = 0; i < array.Length; i++)
-{
-newArray[i] = array[i];
-}
-return newArray;
-}
+// int[] CopyArray(int[] array)
+// {
+// int[] newArray = new int[array.Length];
+// for (int i = 0; i < array.Length; i++)
+// {
+// newArray[i] = array[i];
+// }
+// return newArray;
+// }
 
-int[] CreateArrayRndInt(int size, int min, int max)
-{
-int[] array = new int[size];
-Random random = new Random();
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// Random random = new Random();
 
-for(int i = 0; i < array.Length; i++)
-{
-array[i] = random.Next(min, max +1);
-}
+// for(int i = 0; i < array.Length; i++)
+// {
+// array[i] = random.Next(min, max +1);
+// }
 
-return array;
-}
+// return array;
+// }
 
-void PrintArray
+// void PrintArray
