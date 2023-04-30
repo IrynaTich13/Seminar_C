11c45e30b466b6c3043b241b2a97e1cfbb271af3
Iryna <tichaya.13@gmail.com>
2023-05-01 00:14:23 +0300

    Решение Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -7,3 +7,37 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+
+double[,] CreateMatrixRndDouble(int rows, int columns, double max, double min, int round = 1)   //int round = 1 - можно тоже добавлять этот параметр
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+
+        {
+            double num = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(num, 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2d = CreateMatrixRndDouble(3, 4, -100, 100);
+PrintMatrixDouble(array2d);
