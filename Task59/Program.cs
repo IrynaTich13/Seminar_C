69a6f6758910cd43d82d71fcfc553c57c51f8857
Iryna <tichaya.13@gmail.com>
2023-05-28 13:50:44 +0300

    Решение Task59

--- Task59/Program.cs
+++ Task59/Program.cs
@@ -48,23 +48,30 @@ int[] СoordinatesMinimumValue(int[,] matrix)
             }
         }
     }
-    return new int[] {minIndexRow, minIndexColumn};
+    return new int[] { minIndexRow, minIndexColumn };
 }
 
-int[,] NewMatrix(int[,] matrix, int[] coords)
+int[,] MatrixWithoutRowColumnMinValue(int[,] matrix, int[] coords)
 {
-    int[,] newMatrix = new int[matrix.GetLength(0) -1, matrix.GetLength(1) -1];
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    for (int i = 0, newRow = 0; i < matrix.GetLength(0); i++)
     {
         if (i == coords[0]) continue;
+        for (int j = 0, newColumn = 0; j < matrix.GetLength(1); j++)
         {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-             if (j   == coords[0]) continue;
-            }
+            if (j == coords[0]) continue;
+            newMatrix[newRow, newColumn] = matrix[i, j];
+            newColumn ++;
         }
+        newRow ++;
     }
-    {
-
-    }
+    return newMatrix;
 }
+
+
+int[,] array2d = CreateMatrix(4, 5, -10, 10);
+PrintMatrix(array2d);
+int[] coordinates = СoordinatesMinimumValue(array2d);
+int[,] newArray2d = MatrixWithoutRowColumnMinValue(array2d, coordinates);
+Console.WriteLine();
+PrintMatrix(newArray2d);
