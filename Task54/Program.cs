92f9afdef2cdd04714e3b6429809a66055520eb9
Iryna <tichaya.13@gmail.com>
2023-05-29 00:05:24 +0300

    Решение Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -10,3 +10,66 @@
 // 9 5 3 2
 // 8 4 4 2
 
+
+int[,] array2d = CreateMatrix(3, 4, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+SortDescendingElementRow(array2d);
+PrintMatrix(array2d);
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{ // 0 1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SortDescendingElementRow(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if(matrix[i,j] <= matrix[i,k]) continue;
+                int temp = 0;
+                temp = matrix[i,j];
+                matrix[i,j] = matrix[i,k];
+                matrix[i,k] = temp;
+                
+            }
+        }
+    }
+}
+
+// int[,] array2d = CreateMatrix(3, 4, 0, 10);
+// PrintMatrix(array2d);
+// Console.WriteLine();
+// SortDescendingElementRow(array2d);
+// PrintMatrix(array2d);
