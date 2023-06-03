3f8ff2f6d996701ef24af99f41c95a5ac8137d1d
Iryna <tichaya.13@gmail.com>
2023-06-03 16:14:29 +0300

    Решение Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine($"Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+ void NaturalNumbers(int num)
+ {
+    if(num ==0) return;  // определили условия выхода из рекурсии. Для этого переменная должна меняться.
+    NaturalNumbers (num - 1);
+    Console.Write($"{num} ");
+ }
---
+++ Task65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+// не забываем проверку! М может быть больше N, может быть меньше N, может быть равно N
+
+
+Console.WriteLine($"Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите последнее число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersRange(m, n);
+
+void NaturalNumbersRange(int start, int finish)
+{
+    //Console.Write($"{start } ");
+    if (start < finish)
+    {
+        Console.Write($"{start} ");
+        NaturalNumbersRange(start + 1, finish);
+    }
+    else if (start > finish)
+    {
+        Console.Write($"{start} ");
+        NaturalNumbersRange(start - 1, finish);
+    }
+    else
+    {
+        Console.WriteLine(start);
+    }
+}
